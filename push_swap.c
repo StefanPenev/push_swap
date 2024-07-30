6be5e8dcf8ea054b862009b5b508194c9a4dfc5f
StefanPenev <stfnpenev@gmail.com>
2024-07-30 22:05:18 +0200

    norm

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: stefan <stefan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 14:59:47 by stefan            #+#    #+#             */
-/*   Updated: 2024/07/30 14:38:21 by stefan           ###   ########.fr       */
+/*   Updated: 2024/07/30 22:03:51 by stefan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,40 +37,87 @@ int	count_nums(char **values)
 	return (i);
 }
 
+void process_arguments(int argc, char *argv[], int *count, char ***values)
+{
+    if (argc == 2)
+    {
+        *values = ft_split(argv[1], ' ');
+        *count = count_nums(*values);
+    }
+    else
+    {
+        *values = argv + 1;
+        *count = argc - 1;
+    }
+    validate_arguments(*count, *values);
+}
+
+void check_and_sort(int argc, int count, t_lst **stack_a, t_lst **stack_b)
+{
+    if (is_stack_sorted(*stack_a))
+    {
+        free_list(*stack_a);
+        exit(EXIT_SUCCESS);
+    }
+    if (argc == 4 || count <= 3)
+    {
+        mini_sort(stack_a);
+        free_list(*stack_a);
+        exit(EXIT_SUCCESS);
+    }
+    initial_sort(stack_a, stack_b);
+}
+
 void	push_swap(int argc, char *argv[])
 {
-	t_lst	*stack_a;
-	t_lst	*stack_b;
-	char	**values;
-	int		count;
+    t_lst	*stack_a;
+    t_lst	*stack_b;
+    char	**values;
+    int		count;
 
 	stack_a = NULL;
-	stack_b = NULL;
-	count = 0;
-	if (argc == 2)
-	{
-		values = ft_split(argv[1], ' ');
-		count = count_nums(values);
-	}
-	else
-	{
-		values = argv + 1;
-		count = argc - 1;
-	}
-	validate_arguments(count, values);
-	init_stack(&stack_a, count, values);
-	if (is_stack_sorted(stack_a))
-	{
-		free_list(stack_a);
-		exit(EXIT_SUCCESS);
-	}
-	if (argc == 4 || count <= 3)
-	{
-		mini_sort(&stack_a);
-		free_list(stack_a);
-		exit(EXIT_SUCCESS);
-	}
-	initial_sort(&stack_a, &stack_b);
-	free_list(stack_a);
-	free_list(stack_b);
+    stack_b = NULL;
+    process_arguments(argc, argv, &count, &values);
+    init_stack(&stack_a, count, values);
+    check_and_sort(argc, count, &stack_a, &stack_b);
+    free_list(stack_a);
+    free_list(stack_b);
 }
+
+// void	push_swap(int argc, char *argv[])
+// {
+// 	t_lst	*stack_a;
+// 	t_lst	*stack_b;
+// 	char	**values;
+// 	int		count;
+
+// 	stack_a = NULL;
+// 	stack_b = NULL;
+// 	count = 0;
+// 	if (argc == 2)
+// 	{
+// 		values = ft_split(argv[1], ' ');
+// 		count = count_nums(values);
+// 	}
+// 	else
+// 	{
+// 		values = argv + 1;
+// 		count = argc - 1;
+// 	}
+// 	validate_arguments(count, values);
+// 	init_stack(&stack_a, count, values);
+// 	if (is_stack_sorted(stack_a))
+// 	{
+// 		free_list(stack_a);
+// 		exit(EXIT_SUCCESS);
+// 	}
+// 	if (argc == 4 || count <= 3)
+// 	{
+// 		mini_sort(&stack_a);
+// 		free_list(stack_a);
+// 		exit(EXIT_SUCCESS);
+// 	}
+// 	initial_sort(&stack_a, &stack_b);
+// 	free_list(stack_a);
+// 	free_list(stack_b);
+// }
